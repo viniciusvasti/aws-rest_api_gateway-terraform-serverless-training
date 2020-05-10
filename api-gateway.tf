bd6c8e7f11fa4a08f9c4bf85a38855d97b638c3c
Vinícius A. dos Santos <vinicius.andrade@riachuelo.com.br>
2020-05-10 11:26:21 -0300

    Criar recursos na api "minha-api" e "v1"

--- api-gateway.tf
+++ api-gateway.tf
@@ -6,3 +6,15 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_resource" "minha-api" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "minha-api"
+}
+
+resource "aws_api_gateway_resource" "v1" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_resource.minha-api.id
+  path_part   = "v1"
+}

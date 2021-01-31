31db948ef14a0453f0027357752699b9e5574cc8
Vinicius A Santos <vinicius.andrade@riachuelo.com.br>
2021-01-31 12:22:24 -0300

    Change to english

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,20 +1,20 @@
 # api-gateway.tf
 resource "aws_api_gateway_rest_api" "api_gateway" {
   name        = "Minha API"
-  description = "AWS Rest API de exemplo com Terraform"
+  description = "AWS Rest API example with Terraform"
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-resource "aws_api_gateway_resource" "minha-api" {
+resource "aws_api_gateway_resource" "my-api" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
   parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
-  path_part   = "minha-api"
+  path_part   = "my-api"
 }
 
 resource "aws_api_gateway_resource" "v1" {
   rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_resource.minha-api.id
+  parent_id   = aws_api_gateway_resource.my-api.id
   path_part   = "v1"
 }
--- handler.js
+++ handler.js
@@ -1,27 +1,26 @@
-'use strict';
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
 module.exports.hello = async (event) => {
-  AWS.config.update({ region: 'us-east-1' });
-  try {
-    const { phoneNumber, message } = JSON.parse(event.body);
-    await new AWS.SNS({ apiVersion: '2010-03-31' })
-      .publish({ Message: message, PhoneNumber: `+55${phoneNumber}` })
-      .promise();
-    return {
-      statusCode: 201,
-      body: JSON.stringify(
-        {
-          message: 'SMS enviado com sucesso!',
-        },
-        null,
-        2
-      ),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error),
-    };
-  }
+    AWS.config.update({ region: "us-east-1" });
+    try {
+        const { phoneNumber, message } = JSON.parse(event.body);
+        await new AWS.SNS({ apiVersion: "2010-03-31" })
+            .publish({ Message: message, PhoneNumber: `+55${phoneNumber}` })
+            .promise();
+        return {
+            statusCode: 201,
+            body: JSON.stringify(
+                {
+                    message: "SMS sent!",
+                },
+                null,
+                2
+            ),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error),
+        };
+    }
 };

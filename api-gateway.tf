862d2829c330f42655e87cc0d299172647930bad
Vinícius A. dos Santos <vinicius.vas.ti@gmail.com>
2021-02-01 20:08:40 -0300

    Update api-gateway.tf

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,5 +1,5 @@
 # api-gateway.tf
-resource "aws_api_gateway_rest_api" "api_gateway" {
+resource "aws_api_gateway_rest_api" "my_api_gateway" {
   name        = "Minha API"
   description = "AWS Rest API example with Terraform"
   endpoint_configuration {
@@ -7,14 +7,14 @@ resource "aws_api_gateway_rest_api" "api_gateway" {
   }
 }
 
-resource "aws_api_gateway_resource" "my-api" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+resource "aws_api_gateway_resource" "my_api" {
+  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.my_api_gateway.root_resource_id
   path_part   = "my-api"
 }
 
 resource "aws_api_gateway_resource" "v1" {
-  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
-  parent_id   = aws_api_gateway_resource.my-api.id
+  rest_api_id = aws_api_gateway_rest_api.my_api_gateway.id
+  parent_id   = aws_api_gateway_resource.my_api.id
   path_part   = "v1"
 }
